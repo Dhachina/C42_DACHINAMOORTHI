4da4c8e5e57f71c6c70f40a94850f144560b5013
dhach <dhach@acer>
2024-08-24 20:48:58 +0530

    commit 3

---
+++ C42_DHACHINAMOORTHI/src/com/tnsif/Day2/Basic/inheritance.java
@@ -0,0 +1,15 @@
+package com.tnsif.Day2.Basic;
+
+public class inheritance {
+	public static void main(String[]args) {
+		
+	}
+}
+class puppy extends dog{
+	void setup()
+	{
+		System.out.println("the dog is barking");	
+		} 
+	}
+class 
+
